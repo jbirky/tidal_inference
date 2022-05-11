543cec100ab3e18e4a691e032761e1ef478f1075
jbirky <jbirky@uw.edu>
2022-05-11 13:44:46 -0700

    config for cluster

--- mcmc/execute.py
+++ mcmc/execute.py
@@ -7,17 +7,22 @@ import alabi
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--file', type=str, required=True)
+parser.add_argument("--file", type=str, required=True)
+parser.add_argument("--method", type=str, default="dynesty")
 args = parser.parse_args()
 
-# Parse config.yaml file
-lnlike, ptform, labels, bounds = mcmc.configure_model(args.file)
+# # Parse config.yaml file
+# lnlike, ptform, labels, bounds = mcmc.configure_model(args.file)
 
-base_dir = os.path.dirname(os.path.realpath(args.file))
+synth = mcmc.SyntheticModel(args.file)
+
+# base_dir = os.path.dirname(os.path.realpath(args.file))
 config_name = args.file.split("/")[-1].split(".yaml")[0].strip("/")
-savedir = os.path.join(base_dir, config_name)
+savedir = os.path.join("results/", config_name)
 
 # Run dynesty
-sm = alabi.SurrogateModel(fn=lnlike, bounds=bounds, savedir=savedir, labels=labels)
-sm.run_dynesty(like_fn="true", ptform=ptform, mode="dynamic")
-sm.plot(plots=["dynesty_all"])
+sm = alabi.SurrogateModel(fn=synth.lnlike, bounds=synth.bounds, savedir=savedir, labels=synth.labels)
+
+if args.method == "dynesty":
+    sm.run_dynesty(like_fn="true", ptform=synth.ptform, mode="dynamic")
+    sm.plot(plots=["dynesty_all"])
