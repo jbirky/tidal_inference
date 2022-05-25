58673db2e970318e1f8044b1e24f659220e677ae
jbirky <jbirky@uw.edu>
2022-05-25 16:22:57 -0700

    plot fixes

--- src/analysis.py
+++ src/analysis.py
@@ -28,12 +28,12 @@ class SyntheticModel(object):
         self.cfile = cfile
         self.config_id = self.cfile.split("/")[-1].split(".yaml")[0].strip("/")
 
-        if outpath is None:
-            self.outpath = os.path.join("results", self.config_id)
-        else: 
-            self.outpath = outpath
-        if not os.path.exists(self.outpath):
-            os.makedirs(self.outpath)
+        # if outpath is None:
+        #     self.outpath = os.path.join("results", self.config_id)
+        # else: 
+        #     self.outpath = outpath
+        # if not os.path.exists(self.outpath):
+        #     os.makedirs(self.outpath)
     
         # load YAML config file
         with open(self.cfile) as f:
@@ -111,7 +111,7 @@ class SyntheticModel(object):
         theta_run = self.format_theta(theta_var)
 
         # run model
-        output = self.vpm.run_model(theta_run)
+        output = self.vpm.run_model(theta_run, remove=True)
 
         return output
 
@@ -151,10 +151,10 @@ class SyntheticModel(object):
         import alabi
 
         if method == "dynesty":
-            savedir = os.path.join("dynesty/", self.config_id)
+            savedir = os.path.join("results_dynesty/", self.config_id)
 
         elif method == "alabi":
-            savedir = os.path.join("alabi/", self.config_id)
+            savedir = os.path.join("results_alabi/", self.config_id)
 
         # set up prior for dynesty
         self.ptform = partial(alabi.utility.prior_transform_normal, bounds=self.bounds, data=self.prior_data)
@@ -200,7 +200,11 @@ class SyntheticModel(object):
         if Y is None:
             Y = self.run_models(param_values)
 
-        np.savez(self.outpath + "var_global_sensitivity_sample.npz", param_values=param_values, Y=Y)
+        # save samples to npz file
+        savedir = os.path.join("results_sensitivity", self.config_id)
+        if not os.path.exists(savedir):
+            os.makedirs(savedir)
+        np.savez(f"{savedir}/var_global_sensitivity_sample.npz", param_values=param_values, Y=Y)
 
         dict_s1 = {'input':self.inparams_var.keys()}
         dict_sT = {'input':self.inparams_var.keys()}
@@ -211,28 +215,30 @@ class SyntheticModel(object):
             dict_sT[list(self.outparams.keys())[ii]] = res['ST']
             
         table_s1 = pd.DataFrame(data=dict_s1)
-        table_s1 = table_s1.set_index("input").round(2)
+        table_s1 = table_s1.set_index("input").rename_axis(None, axis=0).round(2)
         table_s1[table_s1.values <= 0] = 0
         table_s1[table_s1.values > 1] = 1
         self.table_s1 = table_s1
 
         table_sT = pd.DataFrame(data=dict_sT)
-        table_sT = table_sT.set_index("input").round(2)
+        table_sT = table_sT.set_index("input").rename_axis(None, axis=0).round(2)
         table_sT[table_sT.values <= 0] = 0
         table_sT[table_sT.values > 1] = 1
         self.table_sT = table_sT
 
+        # plot sensitivity tables
+        self.fig_s1 = plt.figure(figsize=[12,8])
+        sn.heatmap(table_s1, annot=True, annot_kws={"size": 18}) 
+        plt.title("First order sensitivity (S1) index", fontsize=25)
+        plt.xticks(rotation=45, fontsize=18, ha='right')
+        plt.yticks(fontsize=18)
+        plt.savefig(f"{savedir}/sensitivity_table_s1.png", bbox_inches="tight")
+        plt.close()
 
-    def show_variance_table(self, table="s1"):
-
-        if table == "s1":
-            df = self.table_s1
-        elif table == "sT":
-            df = self.table_sT
-
-        fig = plt.figure(figsize=[12,8])
-        sn.heatmap(df, annot=True, annot_kws={"size": 22}) 
-        plt.xticks(rotation=45)
-        plt.show()
-
-        return fig
+        self.fig_sT = plt.figure(figsize=[12,8])
+        sn.heatmap(table_sT, annot=True, annot_kws={"size": 18}) 
+        plt.title("Total sensitivity (ST) index", fontsize=25)
+        plt.xticks(rotation=45, fontsize=18, ha='right')
+        plt.yticks(fontsize=18)
+        plt.savefig(f"{savedir}/sensitivity_table_sT.png", bbox_inches="tight")
+        plt.close()
