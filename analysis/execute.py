35f9de45e8124a2806b0a09497362dac3df11004
jbirky <jbirky@uw.edu>
2022-05-25 22:34:06 -0700

    add opts

--- analysis/execute.py
+++ analysis/execute.py
@@ -1,5 +1,6 @@
 import argparse
 from functools import partial
+import multiprocessing as mp
 import os
 import sys
 sys.path.append(os.path.realpath("../src"))
@@ -13,10 +14,11 @@ parser.add_argument("--operation", type=str, required=True)
 parser.add_argument("--method", type=str, default="dynesty")
 parser.add_argument("--reload", type=bool, default=False)
 parser.add_argument("--niter", type=int, default=500)
+parser.add_argument("--ncore", type=int, default=mp.cpu_count())
 args = parser.parse_args()
 
 # Parse config.yaml file
-synth = analysis.SyntheticModel(args.file, verbose=True)
+synth = analysis.SyntheticModel(args.file, verbose=False, ncore=args.ncore)
 
 if args.operation == "sensitivity":
     synth.variance_global_sensitivity()
