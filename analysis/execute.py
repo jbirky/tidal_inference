5abc4e58d40de9ee0f30dd493e7697bf89bad3c1
jbirky <jbirky@uw.edu>
2022-05-26 12:09:12 -0700

    mp nice opt

--- analysis/execute.py
+++ analysis/execute.py
@@ -12,16 +12,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--file", type=str, required=True)
 parser.add_argument("--operation", type=str, required=True)
 parser.add_argument("--method", type=str, default="dynesty")
+
+# sensitivity options
+parser.add_argument("--nsample", type=int, default=1024)
+
+# alabi options
 parser.add_argument("--reload", type=bool, default=False)
 parser.add_argument("--niter", type=int, default=500)
+
+# multiprocessing options
 parser.add_argument("--ncore", type=int, default=mp.cpu_count())
+parser.add_argument("--nice", type=int, default=10)
+
 args = parser.parse_args()
+os.nice(args.nice)
 
 # Parse config.yaml file
 synth = analysis.SyntheticModel(args.file, verbose=False, ncore=args.ncore)
 
-if args.operation == "sensitivity":
-    synth.variance_global_sensitivity()
+if args.operation.lower()[0:4] == "sens":
+    synth.variance_global_sensitivity(nsample=args.nsample)
 
-elif args.operation == "mcmc":
+elif args.operation.lower() == "mcmc":
     synth.run_mcmc(method=args.method, reload=args.reload, niter=args.niter)
