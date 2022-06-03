edd28891164ef0eba693823774c841a45e78a150
jbirky <jbirky@uw.edu>
2022-06-02 22:07:38 -0700

    figure fixes

--- analysis/batch.py
+++ analysis/batch.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import re
 import os
 import sys
 sys.path.append(os.path.realpath("../src"))
@@ -35,24 +36,32 @@ def get_tables_sorted(config_id_list):
         age, tab = get_table_s1(cid)
         ages.append(age)
         tables.append(tab)
+
+    # sens_params = synth.outparams.names
+    sens_params = ["final.primary.RotPer", 
+                   "final.secondary.OrbPeriod",
+                   "final.secondary.Eccentricity"]
         
     synth = analysis.SyntheticModel(f"config/config_{config_id_list[0]}.yaml", verbose=False, compute_true=False)
-    tsort = dict(zip(synth.outparams.names, [{} for ii in range(synth.outparams.num)]))
+    tsort = dict(zip(sens_params, [{} for ii in range(synth.outparams.num)]))
 
     for ii, tab in enumerate(tables):
         for key in tsort:
             tsort[key][str(int(ages[ii]))] = tab[key]
         
-    return tsort, synth.inparams_var.labels
+    inlabels = synth.inparams_var.labels
+    outlabels = [synth.outparams.dict_labels[par] for par in sens_params]
+    
+    return tsort, inlabels, outlabels
 
 
-def plot_tables_sorted(tsort, labels, model="", save=False, show=True, cmap="bone"):
+def plot_tables_sorted(tsort, inlabels, outlabels, model="", save=False, show=True, cmap="bone"):
     
     for key in tsort.keys():
         df = pd.DataFrame(data=tsort[key])
         
-        sn.heatmap(df, yticklabels=labels, annot=True, annot_kws={"size": 16}, vmin=0, vmax=1, cmap=cmap)
-        plt.title(key + " variance fraction", fontsize=22)
+        sn.heatmap(df, yticklabels=inlabels, annot=True, annot_kws={"size": 16}, vmin=0, vmax=1, cmap=cmap)
+        plt.title(outlabels[ii] + " sensitivity", fontsize=22)
         plt.yticks(rotation=0)
         plt.xlabel("Age [Myr]", fontsize=22)
         plt.ylabel("Initial Conditions", fontsize=22)
@@ -61,22 +70,39 @@ def plot_tables_sorted(tsort, labels, model="", save=False, show=True, cmap="bon
         if show == True:
             plt.show()
         plt.close()
+        
+
+def plot_tables_sorted_combined(tsort, inlabels, outlabels, model="", save=False, show=True, cmap="bone"):
+    fig, axs = plt.subplots(1, 3, figsize=[20,6], sharey=True)
+    plt.subplots_adjust(wspace=.01)
+
+    for ii, key in enumerate(tsort.keys()):
+        df = pd.DataFrame(data=tsort[key])
+        sn.heatmap(df, yticklabels=inlabels, annot=True, annot_kws={"size": 14}, cbar=False, ax=axs[ii], vmin=0, vmax=1, cmap=cmap)
+        axs[ii].set_title(outlabels[ii] + " sensitivity", fontsize=20)
+    axs[1].set_xlabel("Age [Myr]", fontsize=20)
+    axs[0].set_ylabel(model.upper() + "  initial conditions", fontsize=20)
+
+    if save == True:
+        plt.savefig(f"../draft/figures/sensitivity_{model}.png", bbox_inches="tight")
+    if show == True:
+        plt.show()
+    plt.close()
 
 
 ctl = ["050", "051", "052", "053", "054", "055", "056"]
 cpl = ["057", "058", "059", "060", "061", "062", "063"]
 
 
-config_ids = "ctl"
+config_ids = "cpl"
 
 # for cid in eval(config_ids):
 #     file = f"config/config_{cid}.yaml"
 #     synth = analysis.SyntheticModel(file, verbose=False)
 #     synth.variance_global_sensitivity(nsample=512)
 
-if "cpl" in config_ids:
-    cmap = "pink"
-else:
-    cmap = "bone"
-tsort, labels = get_tables_sorted(eval(config_ids))
-plot_tables_sorted(tsort, labels, model=config_ids, show=False, save=True, cmap=cmap)
+cmap = "pink" if "cpl" in config_ids else "bone"
+tsort, inlabels, outlabels = get_tables_sorted(eval(config_ids))
+inlabels = [re.sub("[\(\[].*?[\)\]]", "", lbl) for lbl in inlabels]
+outlabels = [re.sub("[\(\[].*?[\)\]]", "", lbl) for lbl in outlabels]
+plot_tables_sorted(tsort, inlabels, outlabels, model=config_ids, show=False, save=True, cmap=cmap)
--- src/analysis.py
+++ src/analysis.py
@@ -24,10 +24,7 @@ class SyntheticModel(vpi.AnalyzeVplanetModel):
                  outpath=".", 
                  verbose=True, 
                  compute_true=True,
-                 ncore=mp.cpu_count(),
-                 fix_tide=True, 
-                 fix_porb=False,
-                 fix_radius=False):
+                 ncore=mp.cpu_count()):
 
         # load YAML config file
         with open(cfile) as f:
@@ -46,46 +43,70 @@ class SyntheticModel(vpi.AnalyzeVplanetModel):
                          ncore=ncore)
 
         # set primary and secondary to same value?
-        self.fix_tide = fix_tide
+        if "secondary.dTidalTau" in self.inparams_fix.names:
+            self.fix_tide = True 
+        elif "secondary.dTidalQ" in self.inparams_fix.names:
+            self.fix_tide = True 
+        else:
+            self.fix_tdie = False
 
         # compute init porb from final porb, ecc, tau, ...
-        self.fix_porb = fix_porb
+        if "secondary.dOrbPeriod" in self.inparams_fix.names:
+            self.fix_porb = True 
+        else:
+            self.fix_porb = False
 
         if data['module'] == "eqtide":
             self.fix_radius = True
         else:
-            self.fix_radius = fix_radius
+            self.fix_radius = False
 
-        # print(f"fix_tide: {self.fix_tide}, fix_porb: {self.fix_porb}, fix_radius: {self.fix_radius}")
+        print(f"fix_tide: {self.fix_tide}, fix_porb: {self.fix_porb}, fix_radius: {self.fix_radius}")
 
 
-    # def compute_porb(self, theta_run):
+    def compute_porb(self, theta_run):
 
-    #     # prot1_f = self.outparams.get_true_units("final.primary.RotPer")
-    #     # prot2_f = self.outparams.get_true_units("final.secondary.RotPer")
-    #     # porb_f = self.outparams.get_true_units("final.secondary.OrbPeriod")
-    #     # ecc_f = self.outparams.get_true_units("final.secondary.Eccentricity")
+        m1 = theta_run["primary.dMass"] * self.inparams_all.dict_units["primary.dMass"]
+        m2 = theta_run["secondary.dMass"] * self.inparams_all.dict_units["secondary.dMass"]
+        r1 = theta_run["primary.dRadius"] * self.inparams_all.dict_units["primary.dRadius"]
+        r2 = theta_run["secondary.dRadius"] * self.inparams_all.dict_units["secondary.dRadius"]
+        prot1_i = theta_run["primary.dRotPeriod"] * self.inparams_all.dict_units["primary.dRotPeriod"]
+        prot2_i = theta_run["secondary.dRotPeriod"] * self.inparams_all.dict_units["secondary.dRotPeriod"]
+        ecc_i = theta_run["secondary.dEcc"] * self.inparams_all.dict_units["secondary.dEcc"]
+        rg1, rg2 = 0.45, 0.45
 
-    #     m1 = theta_run["primary.dMass"] * self.inparams.units["primary.dMass"]
-    #     m2 = theta_run["secondary.dMass"] * self.inparams.units["secondary.dMass"]
-    #     prot1_i = theta_run["primary.dRotPeriod"] * self.inparams.units["primary.dRotPeriod"]
-    #     prot2_i = theta_run["secondary.dRotPeriod"] * self.inparams.units["secondary.dRotPeriod"]
-    #     ecc_i = theta_run["secondary.dEcc"] * self.inparams.units["secondary.dEcc"]
-    #     r1_i = 1.0 * u.Rsun
-    #     r2_i = 1.0 * u.Rsun
-    #     r1_i = 0.5 
-    #     r2_i = 0.5
+        # m1 = self.inparams_all.dict_true["primary.dMass"] * self.inparams_all.dict_units["primary.dMass"]
+        # m2 = self.inparams_all.dict_true["secondary.dMass"] * self.inparams_all.dict_units["secondary.dMass"]
+        # prot1_i = self.inparams_all.dict_true["primary.dRotPeriod"] * self.inparams_all.dict_units["primary.dRotPeriod"]
+        # prot2_i = self.inparams_all.dict_true["secondary.dRotPeriod"] * self.inparams_all.dict_units["secondary.dRotPeriod"]
+        # ecc_i = self.inparams_all.dict_true["secondary.dEcc"] * self.inparams_all.dict_units["secondary.dEcc"]
 
-    #     Jrot1_f = self.outparams.get_true_units("final.primary.RotAngMom")
-    #     Jrot2_f = self.outparams.get_true_units("final.secondary.RotAngMom")
-    #     Jorb_f = self.outparams.get_true_units("final.secondary.OrbAngMom")
-    #     Jtot_f = Jrot1_f + Jrot2_f + Jorb_f
+        # # get initial radius and radius of gyration
+        # stellar_in = {"star.dMass": self.inparams_all.dict_units["primary.dMass"], "vpl.dStopTime": u.Myr}
+        # stellar_out = {"initial.star.Radius": u.Rsun, "initial.star.RadGyra": u.dimensionless_unscaled}
+        # stellar_path = os.path.join(vpi.INFILE_DIR, "stellar")
+        # stel = vpi.VplanetModel(stellar_in, inpath=stellar_path, outparams=stellar_out, verbose=False)
+        # r1_i, rg1_i = stel.run_model([theta_run["primary.dMass"], 5.0])
+        # r2_i, rg2_i = stel.run_model([theta_run["secondary.dMass"], 5.0])
+        # r1_i *= u.Rsun
+        # r2_i *= u.Rsun
 
-    #     Jrot1_i = m1 * (rg1_i * r1_i)**2 * (2*np.pi / prot1_i)
-    #     Jrot2_i = m2 * (rg2_i * r2_i)**2 * (2*np.pi / prot2_i)
-    #     Jorb_i = Jtot_f - Jrot1_i - Jrot2_i
+        Jrot1_f = self.outparams.dict_true["final.primary.RotAngMom"] * self.outparams.dict_units["final.primary.RotAngMom"]
+        Jrot2_f = self.outparams.dict_true["final.secondary.RotAngMom"] * self.outparams.dict_units["final.secondary.RotAngMom"]
+        Jorb_f = self.outparams.dict_true["final.secondary.OrbAngMom"] * self.outparams.dict_units["final.secondary.OrbAngMom"]
+        Jtot_f = Jrot1_f + Jrot2_f + Jorb_f
 
-    #     return 0
+        Jrot1_i = (m1 * (rg1 * r1)**2 * (2*np.pi / prot1_i)).si
+        Jrot2_i = (m2 * (rg2 * r2)**2 * (2*np.pi / prot2_i)).si
+        Jorb_i = Jtot_f - Jrot1_i - Jrot2_i
+
+        a_i = (Jorb_i**2 * (m1 + m2) / (const.G * m1**2 * m2**2 * (1 - ecc_i**2))).si
+        porb_i = (2*np.pi * np.sqrt(a_i**3 / (const.G * (m1 + m2)))).si
+
+        porb_i_value = porb_i.to(self.inparams_all.dict_units["secondary.dOrbPeriod"]).value
+        print(porb_i_value, theta_run["secondary.dOrbPeriod"])
+
+        return porb_i_value
 
 
     def format_theta(self, theta_var):
@@ -98,21 +119,15 @@ class SyntheticModel(vpi.AnalyzeVplanetModel):
 
         if self.fix_tide == True:
             if self.tide_model == "ctl":
-                theta_run['secondary.dTidalTau'] = theta_run['primary.dTidalTau']
+                theta_run["secondary.dTidalTau"] = theta_run["primary.dTidalTau"]
             elif self.tide_model == "cpl":
-                theta_run['secondary.dTidalQ'] = theta_run['primary.dTidalQ']
+                theta_run["secondary.dTidalQ"] = theta_run["primary.dTidalQ"]
 
         if self.fix_radius == True:
-            theta_run['primary.dRadius'] = theta_run['primary.dMass']
-            theta_run['secondary.dRadius'] = theta_run['secondary.dMass']
-
-        # theta_run = self.compute_porb(theta_run)
+            theta_run["primary.dRadius"] = theta_run["primary.dMass"]
+            theta_run["secondary.dRadius"] = theta_run["secondary.dMass"]
 
         # if self.fix_porb == True:
-            # back_model = vpi.VplanetModel(self.inparams_all, 
-            #                               inpath=self.inpath, 
-            #                               outparams={"final.primary.OrbPeriod": u.day},
-            #                               forward=False)
-            # porb_init = back_model.run()
+        #     theta_run["secondary.dOrbPeriod"] = self.compute_porb(theta_run)
 
         return np.array(list(theta_run.values()))
